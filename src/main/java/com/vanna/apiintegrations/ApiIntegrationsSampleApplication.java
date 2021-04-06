a294a302557f805bf17577cbf6fc658157854e39
Sai <sairohit.vangala@vannainfotech.com>
2021-04-06 11:26:39 +0530

    Initial commit

---
+++ src/main/java/com/vanna/apiintegrations/ApiIntegrationsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.vanna.apiintegrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIntegrationsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIntegrationsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanna/apiintegrations/ApiIntegrationsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanna.apiintegrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIntegrationsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
