c3728bc988c71b9ed61f45bf94aa4bade753b4cd
Sai <sairohit.vangala@vannainfotech.com>
2021-04-07 21:11:08 +0530

    Adds logging to console & file

--- src/main/java/com/vanna/apiintegrations/rest/RESTIntegrationClient.java
+++ src/main/java/com/vanna/apiintegrations/rest/RESTIntegrationClient.java
@@ -1,5 +1,7 @@
 package com.vanna.apiintegrations.rest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,12 +11,16 @@ import java.util.List;
 @Service
 public class RESTIntegrationClient {
 
+    private static final Logger LOGGER = LogManager.getLogger(RESTIntegrationClient.class);
+
     @Value("${ws.users.url}")
     private String usersURI;
 
     public List<User> getUsers() {
         RestTemplate restTemplate = new RestTemplate();
         UsersResponse response = restTemplate.getForObject(usersURI+"/users", UsersResponse.class);
+        LOGGER.info("Receiving response for REST API");
+        LOGGER.info("Count of available users: "+response.getData().size());
         return response.getData();
     }
 }
--- src/main/java/com/vanna/apiintegrations/soap/SOAPIntegrationClient.java
+++ src/main/java/com/vanna/apiintegrations/soap/SOAPIntegrationClient.java
@@ -1,15 +1,16 @@
 package com.vanna.apiintegrations.soap;
 
+import com.vanna.apiintegrations.rest.RESTIntegrationClient;
 import com.vanna.apiintegrations.wsdl.countryinformation.CapitalCity;
 import com.vanna.apiintegrations.wsdl.countryinformation.CapitalCityResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class SOAPIntegrationClient extends WebServiceGatewaySupport {
 
-    private final Logger LOGGER = LoggerFactory.getLogger("APIINTEGRATIONS::SOAPINTEGRATIONCLIENT");
+    private static final Logger LOGGER = LogManager.getLogger(SOAPIntegrationClient.class);
 
     @Value("${ws.countries.url}")
     private String countriesURI;
